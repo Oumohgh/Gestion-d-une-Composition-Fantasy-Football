ee73e4b2c0f7f4bf6770a17ad38c1f5c7663db8e
oughlane <mohammedoughlane72@gmail.com>
2025-11-13 11:45:32 +0100

    completer function ajouter un joueur avant de list tous les jouers en sidebar

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+ 
+ let budget = 100
 // Liste de tous les joueurs disponibles
 let joueurs = JSON.parse(localStorage.getItem("joueurs")) || [];
 // Composition actuelle de l’équipe
@@ -8,3 +10,39 @@ defenseurs: [],
 milieux: [],
 attaquants: [],
 };
+//function ajouterJ
+
+let a
+ //function filtragebPost 
+
+
+ //function ajouterJ
+ function AjouterJ(){
+
+   div_row.innerHTML = "";
+    ListJ.forEach(element => {
+
+         let card = ` 
+        <div class="player-card card">
+          <img src="..." class="card-img-top" />
+          <div class="card-body">
+            <h5 class="card-title">Nom</h5>
+            <p class="card-text small">Poste • Pays</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <span class="price">10 $</span>
+              <div>
+                <button class="btn btn-sm btn-success">➕ Ajouter</button>
+                <button class="btn btn-sm btn-outline-secondary">✏</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        --> `
+
+      div_row.innerHTML += card
+
+    });
+
+}
+ 
+      
