9173b028e80892371829f3f9b252c9fb7ce6f299
oughlane <mohammedoughlane72@gmail.com>
2025-11-13 13:42:26 +0100

    structurer la logique de  budget et comment calucer dans quelqu ajoute de joueur

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
- 
- let budget = 100
+ //7di m3a  budget//
+ //let budget = 100
 // Liste de tous les joueurs disponibles
 let joueurs = JSON.parse(localStorage.getItem("joueurs")) || [];
 // Composition actuelle de l’équipe
@@ -10,26 +10,51 @@ defenseurs: [],
 milieux: [],
 attaquants: [],
 };
+let budget = N(localStorage.getItem("budget"))  || 100;
+
+//let budget = N(localStorage.getItem("budget")) || 100;
+
+const budgetValue = document.getElementById("budgetValue");
+afficherBudget();
+let budgetChecker ;
+let budgetrestant ;
+
+let  nombreJou =11;
+let  listJo
+
+
+//jib data json 
+
 //function ajouterJ
 
 let a
  //function filtragebPost 
 
-
+//function f terrain
+function AjouterJouTerrain(){
+    try {
+      AjouterJ()
+      
+      
+    } catch (error) {
+    console.log("ce joueur n a pas ajoutee");  
+    }
+}
  //function ajouterJ
  function AjouterJ(){
-
+ let poste;
    div_row.innerHTML = "";
-    ListJ.forEach(element => {
+    ListJ.forEach(element => { 
 
          let card = ` 
         <div class="player-card card">
-          <img src="..." class="card-img-top" />
+          <img src="${element.photo}" class="card-img-top" />
           <div class="card-body">
-            <h5 class="card-title">Nom</h5>
-            <p class="card-text small">Poste • Pays</p>
+            <h5 class="card-title">${element.Nom}</h5>
+            <p class="card-text small">${element.poste}</p>
+            <p class="card-text small">${element.nationalite}</p>
             <div class="d-flex justify-content-between align-items-center">
-              <span class="price">10 $</span>
+              <span class="price">${element.prix}/span>
               <div>
                 <button class="btn btn-sm btn-success">➕ Ajouter</button>
                 <button class="btn btn-sm btn-outline-secondary">✏</button>
